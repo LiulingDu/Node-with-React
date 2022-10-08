c25fe573c196ca8edfdc57169528a4000f75114c
Liuling Du <amberdu@LiulingdeMacBook-Pro.local>
2022-10-08 15:21:39 -0700

    changed greeting

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const express=require('express');
 const app=express();
 
 app.get('/',(req,res) =>{
-  res.send({ hi: 'there'});
+  res.send({ bye: 'buddy'});
 });
 
 const PORT=process.env.PORT || 8888;
-
 app.listen(PORT);
